8e4a561d661cbb9b8fdb46b05706a3d0bb7ce201
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2021-02-22 13:14:26 -0600

    Agrega prueba unitaria y revisión de estándar de código

--- hola_mundo/tests.py
+++ hola_mundo/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 
 
-
 class TestSmokeTest(TestCase):
 
     def test_uno_mas_uno_igual_a_dos(self):
         self.assertEqual(1 + 1, 2)
+
+    def test_true_es_true(self):
+        self.assertTrue(True)
